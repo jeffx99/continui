7839b7f52d05b92085c3d101508e959d2ae4cc74
Jeffery Opoku-Mensah <mensah@mit.edu>
2022-08-15 23:23:16 -0400

    Added unit tests for penalty and note calculations

--- tests/music_test.cpp
+++ tests/music_test.cpp
@@ -5,10 +5,45 @@
 #include "assert.hpp"
 #include "../src/music.hpp"
 
+void note_addition_with_natural_figure()
+{
+  Note note = {.degree = 3, .accidental = 1, .octave = 2};
+  Figure figure = {5};
+
+  Note result = note + figure;
+
+  EXPECT(7, result.degree);
+  EXPECT(0, result.accidental);
+  EXPECT(2, result.octave);
+}
+
+void note_addition_with_sharp_figure()
+{
+  Note note = {.degree = 1, .accidental = 0, .octave = 3};
+  Figure figure = {4, SHARP};
+
+  Note result = note + figure;
+
+  EXPECT(4, result.degree);
+  EXPECT(1, result.accidental);
+  EXPECT(3, result.octave);
+}
+
+void note_addition_with_increasing_octave()
+{
+  Note note = {.degree = 5, .accidental = 0, .octave = 2};
+  Figure figure = {5};
+
+  Note result = note + figure;
+
+  EXPECT(2, result.degree);
+  EXPECT(0, result.accidental);
+  EXPECT(3, result.octave);
+}
 
 void note_conversion_with_natural_key_and_degree()
 {
-  Key key = {.letter = 'D', .accidental = 0, .mode = Mode::major};
+  Key key = {.letter = 'D', .accidental = 0, .mode = MAJOR};
   Note note = {.degree = 4, .accidental = 0, .octave = 3};
 
   Note result = contextualize(key, note);
@@ -20,7 +55,7 @@ void note_conversion_with_natural_key_and_degree()
 
 void note_conversion_with_flat_key_sharp_degree()
 {
-  Key key = {.letter = 'E', .accidental = -1, .mode = Mode::minor};
+  Key key = {.letter = 'E', .accidental = -1, .mode = MINOR};
   Note note = {.degree = 7, .accidental = 1, .octave = 2};
 
   Note result = contextualize(key, note);
@@ -32,7 +67,7 @@ void note_conversion_with_flat_key_sharp_degree()
 
 void note_conversion_with_sharp_key_sharp_degree()
 {
-  Key key = {.letter = 'F', .accidental = 1, .mode = Mode::minor};
+  Key key = {.letter = 'F', .accidental = 1, .mode = MINOR};
   Note note = {.degree = 3, .accidental = 1, .octave = 3};
 
   Note result = contextualize(key, note);
@@ -83,9 +118,14 @@ void figured_bass_root_with_non_harmonic_bass()
 
 int main(int argc, char **argv)
 {
+  note_addition_with_natural_figure();
+  note_addition_with_sharp_figure();
+  note_addition_with_increasing_octave();
+
   note_conversion_with_natural_key_and_degree();
   note_conversion_with_flat_key_sharp_degree();
   note_conversion_with_sharp_key_sharp_degree();
+
   figured_bass_root_with_triad();
   figured_bass_root_with_seventh();
   figured_bass_root_with_suspension();
---
+++ tests/penalty_test.cpp
@@ -0,0 +1,239 @@
+#include <iostream>
+
+#include "assert.hpp"
+#include "../src/penalty.hpp"
+
+void omission_penalty_with_ambiguous_quality()
+{
+  FiguredBass bass =
+  {
+    .note = {.degree = 5},
+    .figures =
+    {
+      {7},
+      {5},
+      {3}
+    }
+  };
+
+  Harmony harmony =
+  {
+    {.degree = 4, .octave = 3},
+    {.degree = 2, .octave = 3}
+  };
+
+  float penalty = penalties::third_omission_penalty(&harmony, &bass, MAJOR);
+  EXPECT(1, penalty);
+}
+
+void omission_penalty_with_seventh_chord()
+{
+  FiguredBass bass =
+  {
+    .note = {.degree = 4},
+    .figures =
+    {
+      {6},
+      {5},
+      {3}
+    }
+  };
+
+  Harmony harmony =
+  {
+    {.degree = 2, .octave = 3},
+    {.degree = 6, .octave = 2}
+  };
+
+  float penalty = penalties::seventh_omission_penalty(&harmony, &bass, MAJOR);
+  EXPECT(1, penalty);
+}
+
+void omission_penalty_with_ninth()
+{
+  FiguredBass bass =
+  {
+    .note = {.degree = 4},
+    .figures =
+    {
+      {9},
+      {7}
+    }
+  };
+
+  Harmony harmony =
+  {
+    {.degree = 3, .octave = 3},
+    {.degree = 4, .octave = 2}
+  };
+
+  float penalty = penalties::dissonant_omission_penalty(&harmony, &bass, MAJOR);
+  EXPECT(1, penalty);
+}
+
+
+void omission_penalty_with_fourth()
+{
+  FiguredBass bass =
+  {
+    .note = {.degree = 1},
+    .figures =
+    {
+      {5},
+      {4}
+    }
+  };
+
+  Harmony harmony =
+  {
+    {.degree = 5, .octave = 2},
+    {.degree = 5, .octave = 2}
+  };
+
+  float penalty = penalties::dissonant_omission_penalty(&harmony, &bass, MAJOR);
+  EXPECT(1, penalty);
+}
+
+void doubling_penalty_with_doubled_third()
+{
+  FiguredBass bass =
+  {
+    .note = {.degree = 7},
+    .figures =
+    {
+      {6},
+      {5},
+      {3}
+    }
+  };
+
+  Harmony harmony =
+  {
+    {.degree = 7, .octave = 3},
+    {.degree = 7, .octave = 2},
+    {.degree = 5, .octave = 2},
+    {.degree = 4, .octave = 2}
+  };
+
+  float penalty = penalties::third_doubling_penalty(&harmony, &bass, MAJOR);
+  EXPECT(2, penalty);
+}
+
+void doubling_penalty_with_doubled_seventh()
+{
+  FiguredBass bass =
+  {
+    .note = {.degree = 2},
+    .figures =
+    {
+      {6},
+      {4},
+      {3}
+    }
+  };
+
+  Harmony harmony =
+  {
+    {.degree = 7, .octave = 3},
+    {.degree = 4, .octave = 3},
+    {.degree = 5, .octave = 2},
+    {.degree = 4, .octave = 2}
+  };
+
+  float penalty = penalties::seventh_doubling_penalty(&harmony, &bass, MAJOR);
+  EXPECT(1, penalty);
+}
+
+void doubling_penalty_with_doubled_seventh_in_bass()
+{
+  FiguredBass bass =
+  {
+    .note = {.degree = 1},
+    .figures =
+    {
+      {6},
+      {4},
+      {2}
+    }
+  };
+
+  Harmony harmony =
+  {
+    {.degree = 4, .octave = 3},
+    {.degree = 6, .octave = 2},
+    {.degree = 2, .octave = 2},
+    {.degree = 1, .octave = 2}
+  };
+
+  float penalty = penalties::seventh_doubling_penalty(&harmony, &bass, MAJOR);
+  EXPECT(1, penalty);
+}
+
+void motion_penalty_with_parallel_fifths()
+{
+
+}
+
+void motion_penalty_with_parallel_octaves()
+{
+
+}
+
+void motion_penalty_with_contrary_motion()
+{
+
+}
+
+void melodic_penalty_with_perfect_cadence()
+{
+
+}
+
+void melodic_penalty_with_change_of_position()
+{
+
+}
+
+void tendency_penalty_with_perfect_cadence()
+{
+
+}
+
+void tendency_penalty_with_dropped_leading_tone()
+{
+
+}
+
+void tendency_penalty_with_suspension()
+{
+
+}
+
+void tendency_penalty_with_fourth()
+{
+
+}
+
+int main(int argc, char **argv)
+{
+  omission_penalty_with_ambiguous_quality();
+  omission_penalty_with_seventh_chord();
+  omission_penalty_with_ninth();
+  omission_penalty_with_fourth();
+
+  doubling_penalty_with_doubled_third();
+  doubling_penalty_with_doubled_seventh();
+  doubling_penalty_with_doubled_seventh_in_bass();
+
+  motion_penalty_with_parallel_fifths();
+  motion_penalty_with_parallel_octaves();
+  motion_penalty_with_contrary_motion();
+
+  melodic_penalty_with_perfect_cadence();
+  melodic_penalty_with_change_of_position();
+
+  tendency_penalty_with_perfect_cadence();
+  tendency_penalty_with_dropped_leading_tone();
+  tendency_penalty_with_suspension();
+  tendency_penalty_with_fourth();
+}
